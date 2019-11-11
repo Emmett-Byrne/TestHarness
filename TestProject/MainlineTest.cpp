090377cce151d3dc0c260bbf9d9b7864100898a5
unknown <c00220950@itcarlow.ie>
2019-11-11 12:58:19 +0000

    quick fix

--- TestProject/MainlineTest.cpp
+++ TestProject/MainlineTest.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 	Production p;
-	std::vector<int> numbers = { 0,1,2,3,4,5,6 };
+	std::vector<int> numbers = { 0,1,2,3,4,5 };
 
 	assert(numbers.size() == 6);
 	assert(p.checkBetweenRangeZeroToFortySix(numbers) == true);
